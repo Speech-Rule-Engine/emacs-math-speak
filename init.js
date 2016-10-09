112115188ac07ea1ee5b74eca2e6a9ee981a183c
raman <tv.raman.tv@gmail.com>
2016-10-09 13:35:01 -0700

    remove old init.js

--- init.js
+++
@@ -1,8 +0,0 @@
-// Initialize Math rendering system.
-var mjx = require('mathjax-node');
-var sre = require('speech-rule-engine');
-sre.setupEngine({markup: 'acss'});
-
-var runWithCounter = function(counter, callback, args) {  var result = callback.apply(sre, args);
-  console.log('BEGINOUTPUT' + counter + ': ' + result + ' :ENDOUTPUT');
-};
