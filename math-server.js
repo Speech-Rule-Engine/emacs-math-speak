84df5aa9208c34ad63039726db65daab557bd9d7
T. V. Raman <tv.raman.tv@gmail.com>
2016-10-12 15:42:13 -0700

    ran gjslint, and fixjsstyle

--- math-server.js
+++ math-server.js
@@ -28,60 +28,60 @@ var handlers = {};
 
 // Accept a LaTeX math expression:
 handlers.enter = function(expr, socket) {
-    mjx.typeset({math: expr,
-                 format: 'TeX',
-                 mml: true},
-                function(data) {socket.write(sre.walk(data.mml)); }
-               );
+  mjx.typeset({math: expr,
+               format: 'TeX',
+               mml: true},
+              function(data) {socket.write(sre.walk(data.mml)); }
+             );
 };
 
 // Implement Handlers:
-handlers.up = function () {};
-handlers.down = function () {};
-handlers.left = function () {};
-handlers.right = function () {};
-handlers.repeat = function () {};
-handlers.root = function () {};
+handlers.up = function() {};
+handlers.down = function() {};
+handlers.left = function() {};
+handlers.right = function() {};
+handlers.repeat = function() {};
+handlers.root = function() {};
 
 // Start a TCP Server on port 5000
-net.createServer(function (socket) {
-    // Identify this client
-    socket.name = socket.remoteAddress + ":" + socket.remotePort;
-    
-    // Method: respond
-    function respond(message, sender) {
-        // message is of the form:
-        //cmd: args, args, args
-        var request = message.toString();
-        var cmd = request.split(':', 1)[0];
-        var args = request.slice(cmd.length + 1);
-        var handler = handlers[cmd];
-        if (handler !== undefined) {
-          var result = handler.call(null, args, socket);
-            if (result !== undefined ) {
-                sender.write(result);
-            } else {
-                process.stdout.write("Handler " + cmd + " returned undefined. \n");
-            }
-            } else {
-                process.stdout.write("Handler for " + request[0] + " not defined.\n");
-            }
+net.createServer(function(socket) {
+  // Identify this client
+  socket.name = socket.remoteAddress + ':' + socket.remotePort;
+
+  // Method: respond
+  function respond(message, sender) {
+    // message is of the form:
+    //cmd: args, args, args
+    var request = message.toString();
+    var cmd = request.split(':', 1)[0];
+    var args = request.slice(cmd.length + 1);
+    var handler = handlers[cmd];
+    if (handler !== undefined) {
+      var result = handler.call(null, args, socket);
+      if (result !== undefined) {
+        sender.write(result);
+      } else {
+        process.stdout.write('Handler ' + cmd + ' returned undefined. \n');
+      }
+    } else {
+      process.stdout.write('Handler for ' + request[0] + ' not defined.\n');
     }
+  }
 
-        // Announce yourself:
-        socket.write("(Welcome " + socket.name + ")\n");
-        // Handle incoming messages from Emacs:
-        socket.on('data', function (data) {
-            respond(data, socket);
-        });
+  // Announce yourself:
+  socket.write('(Welcome ' + socket.name + ')\n');
+  // Handle incoming messages from Emacs:
+  socket.on('data', function(data) {
+    respond(data, socket);
+  });
 
-        // Shutdown server on disconnect:
-        socket.on('end', function () {
-            socket.destroy();
-            process.exit();
-        });
+  // Shutdown server on disconnect:
+  socket.on('end', function() {
+    socket.destroy();
+    process.exit();
+  });
 
-    }).listen(5000);
+}).listen(5000);
 
-                 // Put a friendly message on the terminal of the server.
-                 console.log("Math server running at port 5000\n");
+// Put a friendly message on the terminal of the server.
+console.log('Math server running at port 5000\n');
