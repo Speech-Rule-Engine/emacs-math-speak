d65ba226f8dc5a19bc21c5c51ec1f3c98446b005
T. V. Raman <tv.raman.tv@gmail.com>
2016-10-17 10:31:13 -0700

    updated ensure

--- emacspeak-maths.el
+++ emacspeak-maths.el
@@ -344,8 +344,8 @@ All complete chunks of output are consumed. Partial output is left for next run.
 
 (defun emacspeak-maths-ensure-server ()
   "Start up Maths Server bridge if not already running."
-  (declare (special emacspeak-maths))
-  (unless (process-live-p (emacspeak-maths-server-process emacspeak-maths))
+  (declare (special emacspeak-maths))s
+  (unless (and emacspeak-maths (process-live-p (emacspeak-maths-server-process emacspeak-maths)))
     (emacspeak-maths-bridge-start)))
 
 (defun emacspeak-maths-restart ()
